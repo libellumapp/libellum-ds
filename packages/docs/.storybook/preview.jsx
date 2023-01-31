776c95c05c0af5fae34bd8cabac145f7b2696b2a
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2023-01-31 10:02:18 -0300

    refactor: 💡 storybook preview

--- packages/docs/.storybook/preview.jsx
+++ packages/docs/.storybook/preview.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { darkMode, lightMode  } from '@libellum-ds/react'
 
+// Define light and dark mode values
 const MODES = [
   {
     name: 'light',
@@ -13,8 +14,33 @@ const MODES = [
     mode: darkMode
   }
 ]
+
+// Define default mode
 const DEFAULT_MODE = MODES[0]
 
+/**
+ * Return background color based on selected storybook background
+ * @param {*} backgrounds
+ * @returns backgroun color
+ */
+const getCurrentBackground = (backgrounds) => {
+  if (!backgrounds) return DEFAULT_MODE.background
+  return ['transparent', DEFAULT_MODE.background].includes(backgrounds.value) ? DEFAULT_MODE.background : backgrounds.value
+}
+
+/**
+ * Return current mode based on selected storybook background
+ * @param {*} backgrounds
+ * @returns current mode
+ */
+const getCurrentMode = (backgrounds) => {
+  const currentModeBackground = getCurrentBackground(backgrounds)
+  return MODES.find((mode) => mode.background === currentModeBackground).mode || ''
+}
+
+/**
+ * Storybook parameters
+ */
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -33,13 +59,18 @@ export const parameters = {
   layout: 'fullscreen'
 }
 
+/**
+ * Storybook global decorators
+ */
 export const decorators = [
   (Story, context) => {
-    const currentModeBackground = context.globals.backgrounds?.value ?? DEFAULT_MODE.background
-    const currentMode = MODES.find((mode) => mode.background === currentModeBackground).mode || ''
+    const currentMode = getCurrentMode(context.globals.backgrounds)
 
     return (
-      <div className={currentMode} style={{ padding: '20px' }}>
+      <div
+        className={currentMode}
+        style={{ padding: '20px' }}
+      >
         <Story />
       </div>
     )
